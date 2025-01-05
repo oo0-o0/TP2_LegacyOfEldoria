76cfcf3b6ad74e05b6cd94e46b0e335b9b7b534f
* ∘ ∙ Érika Liz <erikaliz2014augusto@gmail.com>
2025-01-05 10:12:25 -0300

    FirstCommit

---
+++ ZeldaRecriation/core/src/main/java/game/zelda/MainGame.java
@@ -0,0 +1,37 @@
+package game.zelda;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainGame extends ApplicationAdapter  
+{
+    private SpriteBatch batch;
+    private GameContext gameContext;
+
+    @Override
+    public void create() 
+    {
+        AssetsManager.getInstance().loadAssets();
+        AssetsManager.getInstance().finishLoading();
+
+        batch = new SpriteBatch();
+        gameContext = new GameContext();
+
+        gameContext.setState(new MenuState(gameContext));
+    }
+
+    @Override
+    public void render() 
+    {
+        float delta = com.badlogic.gdx.Gdx.graphics.getDeltaTime();
+        gameContext.update(delta);
+        gameContext.render(batch);
+    }
+
+    @Override
+    public void dispose() 
+    {
+        batch.dispose();
+        AssetsManager.getInstance().dispose();
+    }
+}
